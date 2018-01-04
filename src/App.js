dcb759b94167e6d22ed2ab1e38d47f2bc5aac108
Juan Zamudio <jzamudio14@gmail.com>
2018-01-03 22:46:54 -0600

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <NavBar></NavBar>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/bootstrap.min.css';
+import './css/nucleo-icons.css';
+import './css/paper-kit.css';
+import './css/font-awesome.min.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
