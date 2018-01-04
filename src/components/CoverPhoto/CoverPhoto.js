57ef06f6e473582056bc5155caa617fee150f214
Juan Zamudio <jzamudio14@gmail.com>
2018-01-04 01:23:58 -0600

    Landing page

--- src/components/CoverPhoto/CoverPhoto.js
+++ src/components/CoverPhoto/CoverPhoto.js
@@ -6,6 +6,9 @@ import pomona from '../../images/pomona-icon.ico';
 class CoverPhoto extends Component {
 
   render() {
+	const linkColor = {
+		color: 'white',
+	};
 
     return (
         <div className="page-header section-dark">
@@ -27,14 +30,14 @@ class CoverPhoto extends Component {
 					</h2>
     			</div>
     		</div>
-            <div className="moving-clouds">
-
-            </div>
+            <div className="moving-clouds"></div>
     		<h6 className="category category-absolute">
-			603.682.2835 | jzamudio14@gmail.com | Claremont, California
-    			<a href="https://www.creative-tim.com" target="_blank">
-    				<img src={pomona} className="creative-tim-logo"></img>
-    			</a>
+				<a style={linkColor} href="tel:+16036822835">603.682.2835</a> | 
+				<a style={linkColor} href="mailto:jzamudio14@gmail.com"> jzamudio14@gmail.com</a> | 
+				<a style={linkColor} href="https://www.google.com/maps/place/Claremont,+CA/@34.1223595,-117.7493742,13z/data=!3m1!4b1!4m5!3m4!1s0x80c33a947ba7f65f:0xd57add892abc374d!8m2!3d34.0966764!4d-117.7197785"> Claremont, California</a>
+				{/* <a href="https://www.creative-tim.com" target="_blank">
+					<img src={pomona} className="creative-tim-logo"></img>
+				</a> */}
     		</h6>
     	</div>
     );
